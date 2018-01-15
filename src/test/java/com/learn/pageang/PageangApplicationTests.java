05010bb0c1af48298f407960b0eb3acbefdb7866
Biswajit Debnath <biswajit@ipvisionsoft.com>
2018-01-15 10:37:54 +0600

    TestResrouceProperties added

--- src/test/java/com/learn/pageang/PageangApplicationTests.java
+++ src/test/java/com/learn/pageang/PageangApplicationTests.java
@@ -5,11 +5,13 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(locations="classpath:application-test.properties")
 public class PageangApplicationTests {
 
 	@Test
